437edd3333023c14e6671f9eb0ce2afabe242cc3
sahil thakur <sahilthakurxyz@gmail.com>
2023-04-13 21:13:03 +0530

    Updates

---
+++ precache-manifest.29b6eeb85bf34a8d8c5d94904867693b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6b8d7adc213c5ed5e55",
+    "url": "/new-amazon/static/css/main.5c857aca.chunk.css"
+  },
+  {
+    "revision": "e6b8d7adc213c5ed5e55",
+    "url": "/new-amazon/static/js/main.fc83df5e.chunk.js"
+  },
+  {
+    "revision": "594e2f5d943389eb4ea7",
+    "url": "/new-amazon/static/js/runtime~main.87213113.js"
+  },
+  {
+    "revision": "37ab8c414b7a902d9521",
+    "url": "/new-amazon/static/js/2.fa94eaa4.chunk.js"
+  },
+  {
+    "revision": "8f364c4be8487f8b3f2a",
+    "url": "/new-amazon/static/js/3.3ccd49e2.chunk.js"
+  },
+  {
+    "revision": "e712bac5b07ad3d351ae01a0ae8c0163",
+    "url": "/new-amazon/index.html"
+  }
+];
